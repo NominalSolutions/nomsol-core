770ec47da1dada18f2515b26b94f48f1c970db63
HernJer <hernjer@icloud.com>
2025-04-11 09:52:24 -0500

    Update version and enhance HTTP request handling
    - Bump project version to 1.0.0 for significant release.
    - Add `Authenticator` class for OAuth token management.
    - Introduce `TokenResponse` class for token response structure.
    - Create new classes in `httpcallmodel.cs` for modeling HTTP requests.
    - Enhance `HttpRequestHandler` to support multiple authentication types.
    - Implement `TriggerHttpRequest` method for sending requests and handling responses.

---
+++ nomsol.core/httprequest/authenticator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace nomsol.core.httprequest
+{
+    public class Authenticator
+    {
+        private readonly string _baseUrl;
+        private readonly string _endPoint;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _authScope;
+        private readonly string _grantType;
+        private string Token { get; set; }
+
+        public Authenticator(string baseUrl, string endPoint, string clientId, string clientSecret, string authScope, string grantType = "client_credentials")
+        {
+            _baseUrl = baseUrl;
+            _endPoint = endPoint;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _authScope = authScope;
+            _grantType = grantType;
+        }
+
+        public async Task<string> GetAuthenticationHeaderAsync()
+        {
+            var token = string.IsNullOrEmpty(Token) ? await GetToken() : Token;
+            token = token.StartsWith("Bearer ")
+                            ? token.Replace("Bearer ", string.Empty)
+                            : token;
+            return token;
+        }
+
+        private async Task<string> GetToken()
+        {
+            string tokenEndpoint = $"{_baseUrl}{_endPoint}";
+
+            using var client = new HttpClient();
+            var parameters = new Dictionary<string, string>
+            {
+                { "client_id", _clientId },
+                { "client_secret", _clientSecret },
+                { "scope", _authScope },
+                { "grant_type", _grantType }
+            };
+
+            var content = new FormUrlEncodedContent(parameters);
+            HttpResponseMessage response = await client.PostAsync(tokenEndpoint, content);
+
+            if (!response.IsSuccessStatusCode)
+                throw new InvalidOperationException("Failed to obtain token.");
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(responseContent);
+
+            Token = tokenResponse != null ? $"{tokenResponse.TokenType} {tokenResponse.AccessToken}" : throw new InvalidOperationException("Invalid token response.");
+            return Token;
+        }
+    }
+
+    public class TokenResponse
+    {
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; private set; }
+
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; private set; }
+
+        public TokenResponse(string tokenType, string accessToken)
+        {
+            TokenType = tokenType;
+            AccessToken = accessToken;
+        }
+
+        // Implement value-based equality
+        public override bool Equals(object obj)
+        {
+            return obj is TokenResponse other &&
+                   TokenType == other.TokenType &&
+                   AccessToken == other.AccessToken;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(TokenType, AccessToken);
+        }
+
+        // Method to mimic 'with' functionality
+        public TokenResponse With(string tokenType = null, string accessToken = null)
+        {
+            return new TokenResponse(tokenType ?? TokenType, accessToken ?? AccessToken);
+        }
+    }
+}
---
+++ nomsol.core/httprequest/httpcallmodel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace nomsol.core.httprequest.models
+{
+    public class HttpCallModel
+    {
+        public string Url { get; set; }
+        public string EndPoint { get; set; }
+        public HttpMethod Method { get; set; }
+        public Authenticator Authenticator { get; set; }
+        public List<RequestParameters>? Parameters { get; set; }
+    }
+
+    public class Authenticator
+    {
+        public ApiAuthType AuthType { get; set; }
+        public OAuthCredentials? OAuthCredentials { get; set; }
+        public string? Token { get; set; }
+        public string? User { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public enum ApiAuthType
+    {
+        Header,
+        Basic,
+        Jwt,
+        Oauth,
+        None
+    }
+
+    public class RequestParameters
+    {
+        public ParameterType Type { get; set; }
+        public Dictionary<string, string>? Values { get; set; }
+        public object? requestBody { get; set; }
+    }
+
+    public enum ParameterType
+    {
+        Header,
+        QueryString,
+        RequestBody
+    }
+
+    public class HttpResponse
+    {
+        public string? Message { get; set; }
+    }
+
+    public class OAuthCredentials
+    {
+        public string? baseUrl { get; set; }
+        public string? endPoint { get; set; }
+        public string? clientId { get; set; }
+        public string? clientSecret { get; set; }
+        public string? authScope { get; set; }
+    }
+}
